6274fac6d0cd950a4f4a4105078855634df5f24d
Pholith <victoire22062@gmail.com>
2026-03-28 18:15:12 +0100

    Added some missing strings

--- GodotScriptParser/Program.cs
+++ GodotScriptParser/Program.cs
@@ -42,10 +42,26 @@ static void Main()
         foreach (var message in allMessages)
         {
             if (string.IsNullOrWhiteSpace(message)) continue;
-            sw.WriteLine($"gettext('{message}')");
+            sw.WriteLine(writeAsGetText(message));
         }
+        // Idk why but this is not formatted like others dialogues messages. So I just add them here.
+        sw.WriteLine(writeAsGetText("The Acorn diner, A dirty little burger joint which serves as a hangout for locals and a hub for passer-throughs."));
+        sw.WriteLine(writeAsGetText("Stairs to some old apartment units."));
+        sw.WriteLine(writeAsGetText("It looks like a vending machine."));
+        sw.WriteLine(writeAsGetText("The classic back ally, a prime spot for shady dealings or just keeping out of sight."));
+        sw.WriteLine(writeAsGetText("The local Robotech shop, for all your droid maintenance needs."));
+        sw.WriteLine(writeAsGetText("She\'s staring at you intently, unabashedly even, from the booth where she\'s been sipping her coffee."));
+        sw.WriteLine(writeAsGetText("The missionary is huge, easily twice your height, but exudes an aura of peacefulness."));
+        sw.WriteLine(writeAsGetText("The greasy waiter does not offer you a smile when he sees you approach."));
+        sw.WriteLine(writeAsGetText("The sight of another android makes you feel weird, especially since this one is just a mindless automaton."));
+        sw.WriteLine(writeAsGetText("She looks excited to see you come in."));
     }
-
 }
+
+static string writeAsGetText(string text)
+{
+    return $"gettext('{text}')";
+}
+
 
 Main();
